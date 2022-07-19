9b5d9e3e001cfd7d68de369fb0826b2a0945de46
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-07-19 16:34:46 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { HashRouter as Router } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import ParticlesBg from "particles-bg";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// https://reactjsexample.com/react-particles-animation-background-component/
+
+ReactDOM.render(
+  <Router>
     <App />
-  </React.StrictMode>
+    <ParticlesBg type="square" bg={true} />
+  </Router>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
