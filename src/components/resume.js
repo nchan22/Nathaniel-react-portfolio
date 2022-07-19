2c33af4fa6d31205962aedaf20452ad09c64b5a8
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-07-19 13:32:32 -0400

    Added resume + download

--- src/components/resume.js
+++ src/components/resume.js
@@ -1,1 +1,18 @@
 import React from "react";
+
+const Resume = () => {
+    return (
+        <React.Fragment>
+            <div className='container'>
+                <div className='row'>
+                    <div className='col'>
+                        <div className='card'>
+                            <div className="header">
+                                <h3>Download Resume: <a href="https://github.com/nchan22/a-react-portfolio-of-mine/raw/main/src/assets/nathaniel-chan-resume.pdf">Nathaniel Chan</a>
+                                </h3>
+                            </div>
+
+
+
+
+
