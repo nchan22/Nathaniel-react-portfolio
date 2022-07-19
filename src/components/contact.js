dfbec447f531eab9aecf494de823a64a62ad05af
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-07-19 01:01:56 -0400

    Update contact.js

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,1 +1,12 @@
 import React from "react";
+
+function Contact() {
+    return (
+      <div className='container'>
+          <Jumbotron fluid className="p-5 mb-0">
+              <Container className="text-center">
+                  <div className="p-md-5">
+                      <h1>Get in Touch</h1>
+                  </div>
+              </Container>
+          </Jumbotron>
