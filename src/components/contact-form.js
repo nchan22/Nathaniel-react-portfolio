029a3bfcc101ee1fd0f539928767fb1598b38652
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-07-19 00:42:54 -0400

    Update contact form

--- src/components/contact-form.js
+++ src/components/contact-form.js
@@ -1,1 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+
+function ContactForm() {
+    const [formState, setFormState] = useState({ name: '', email: '', subject: '', message: '' });
+    const [formMessage, setFormMessage] = useState('');
+  
+    // adds error messages to the form
+    function handleChange(e) {
+      if (e.target.name === 'email') {
+        const isValid = validateEmail(e.target.value);
+        if (!isValid) {
+            setFormMessage('Your email is invalid.');
+          } else {
+            setFormMessage('');
+          }
+      } else {
+        if (!e.target.value.length) {
+          const name =  e.target.name;
+          setFormMessage(`${name.charAt(0).toUpperCase() + name.slice(1)} is required.`);
+        } else {
+          setFormMessage('');
+        }
+      }
