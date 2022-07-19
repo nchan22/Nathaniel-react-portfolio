5228ef12767c9c53547c62679337f5fa8378447b
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-07-19 00:57:07 -0400

    Update contact form

--- src/components/contact-form.js
+++ src/components/contact-form.js
@@ -21,3 +21,18 @@ function ContactForm() {
           setFormMessage('');
         }
       }
+
+       // sends the email or surfaces an error on submission
+  function sendEmail(e) {
+    e.preventDefault();
+    // https://medium.com/@shrivastavasucheta/sending-an-email-using-emailjs-gmail-service-to-be-used-in-javascript-d6eb92ed0c7c
+    // emailjs.sendForm(serviceID, templateID, templateParams, publicKey);
+
+    emailjs.sendForm('gmail', 'template_t4xztml', '#contactForm', '2uz138zu8oKKleFyQ')
+    .then(function(response) {
+      console.log(response.text);
+      setFormMessage("Message sent!");
+    }, function(error) {
+      console.log(error.text);
+      setFormMessage("Your message couldn't be sent. Please email Nathaniel directly at nhchan22@gmail.com");
+    });
