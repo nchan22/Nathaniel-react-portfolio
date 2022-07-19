658beb6ce15c029c173dca20ca0f8e073caf6d7d
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-07-19 01:28:35 -0400

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -20,6 +20,27 @@ function Footer() {
           />
         </a>
       </div>
-     
+      <p className="footer-info">
+        &copy; Nathaniel Chan |{" "}
+        <a
+          href="https://github.com/nchan22/a-react-portfolio-of-mine/raw/main/src/assets/nathaniel-chan-resume.pdf"
+          className="link"
+        >
+          download resume
+        </a>{" "}
+        |{" "}
+        <a href="https://coolors.co/f06543-e8e9eb-e0dfd5-313638-f09d51">
+          color palette
+        </a>{" "}
+        | made with{" "}
+        <img
+          id="react-icon"
+          src="https://img.icons8.com/color/48/000000/react-native.png"
+          alt="React"
+        />
+      </p>
+    </div>
+  );
+}
 
 export default Footer;
