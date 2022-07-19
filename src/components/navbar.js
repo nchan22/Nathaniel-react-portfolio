093c23d84502961589d908face1763b91d1f37b8
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-07-19 01:38:33 -0400

    Update navbar.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,1 +1,14 @@
 import React from "react";
+
+function Navbar() {
+    return (
+        <div className='topnav'>
+            <div className='topnav-left'>
+                <NavLink
+                    activeClassName="navbar__link--active"
+                    className="navbar__link"
+                    to="/about"
+                >
+                    ABOUT
+                </NavLink>
+            </div>
