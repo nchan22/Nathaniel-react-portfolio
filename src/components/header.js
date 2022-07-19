4b208eb16fd7b7d7c6473e577f133cea3947c561
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-07-19 01:37:44 -0400

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -1,1 +1,24 @@
 import React from "react";
+
+function Header() {
+  return (
+    <Router>
+      <NavBar />
+      <Switch>
+        <Route exact path="/">
+          <Redirect to="/about" />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/portfolio" component={Portfolio} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/resume">
+          <Resume />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default Header;
