0c6dda806abddbf528dcf31cff97dd9e39dc5df4
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-07-19 01:27:43 -0400

    Added github link

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,1 +1,17 @@
 import React from "react";
+import "../footer.css";
+
+function Footer() {
+  return (
+    <div className="footer" id="footer">
+      <div className="img-link">
+        <a href="https://github.com/nchan22" target="_blank">
+          <img
+            src="https://img.icons8.com/color/48/000000/github-2.png"
+            alt="Github"
+            className="icon"
+          />
+        </a>
+        
+
+export default Footer;
