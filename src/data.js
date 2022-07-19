b736e0cd9dca9bc2ba89acfd3ab3cb94e50b3d84
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-07-19 16:11:02 -0400

    Added projects to data.js

---
+++ src/data.js
@@ -0,0 +1,43 @@
+import leafItToMe from "./assets/images/leaf-it-to-me.jpg";
+import techKeyThoughts from "./assets/images/tech-blog.jpg";
+import tripIt from "./assets/images/trip-it.jpg";
+import budgetTracker from "./assets/images/track-your-spending.jpg";
+import passwordGenerator from "./assets/images/password-generator.jpg";
+
+export default [
+  {
+    id: 1,
+    name: "Leaf it to me",
+    image: leafItToMe,
+    github: "https://github.com/CaptainNate/captain-nates-crazy-project",
+    deploy: "",
+  },
+  {
+    id: 2,
+    name: "Trip It!",
+    image: tripIt,
+    github: "https://github.com/P1Group5/trip-it",
+    deploy: "https://p1group5.github.io/trip-it/",
+  },
+  {
+    id: 3,
+    name: "Tech-key Thoughts: A Tech Blog",
+    image: techKeyThoughts,
+    github: "https://github.com/nchan22/tech-blog",
+    deploy: "https://tech-blog-techkey-thoughts.herokuapp.com",
+  },
+  {
+    id: 4,
+    name: "Track Your Spending",
+    image: budgetTracker,
+    github: "https://github.com/nchan22/track-your-spending",
+    deploy: "https://track-your-spending.herokuapp.com",
+  },
+  {
+    id: 5,
+    name: "Password Generator",
+    image: passwordGenerator,
+    github: "https://github.com/nchan22/password-generator",
+    deploy: "https://nchan22.github.io/password-generator/",
+  },
+];
